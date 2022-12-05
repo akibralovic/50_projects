6a2c3b8a484218686e0c7fa0fff3915a5ae4f9a9
akibralovic <bralovic.andj@gmail.com>
2022-12-05 16:37:10 +0100

    script
    Signed-off-by: akibralovic <bralovic.andj@gmail.com>

--- random_choice_picker/script.js
+++ random_choice_picker/script.js
@@ -3,4 +3,19 @@ const textarea = document.getElementById('textarea')
 
 textarea.focus()
 
-textarea.addEventListener('keyup', ())
+textarea.addEventListener('keyup', (e) => {
+  createTags(e.target.value)
+})
+
+function createTags(input){
+ const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim()) //filtriramo da za svaki tag, skratimo white space ako to nije jednako sa praznim skringom, zatim manipulisemo nizom, za svaki tag vracamo niz skracenog taga, ynaci ne moze da bude prazan string
+
+ tagsEl.innerHTML = ''
+
+ tags.forEach(tag =>{
+  const tagEl = document.createElement('span')
+  tagEl.classList.add('tag')
+  tagEl.innerText = tag
+  tagsEl.appendChild(tagEl)
+ })
+}
