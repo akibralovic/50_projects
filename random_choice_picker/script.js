1d87fa9c216829bf74af6e067cda87d017a78d99
akibralovic <bralovic.andj@gmail.com>
2022-12-07 16:08:50 +0100

    .
    Signed-off-by: akibralovic <bralovic.andj@gmail.com>

--- random_choice_picker/script.js
+++ random_choice_picker/script.js
@@ -5,10 +5,18 @@ textarea.focus()
 
 textarea.addEventListener('keyup', (e) => {
   createTags(e.target.value)
+
+  if(e.key === 'Enter') {
+      setTimeout(() => {
+        e.target.value = ''
+      }, 10)
+
+      randomSelect()
+  }
 })
 
 function createTags(input){
- const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim()) //filtriramo da za svaki tag, skratimo white space ako to nije jednako sa praznim skringom, zatim manipulisemo nizom, za svaki tag vracamo niz skracenog taga, ynaci ne moze da bude prazan string
+ const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim()) //filtriramo da za svaki tag, sklanjamo white space ako to nije jednako sa praznim skringom, zatim manipulisemo nizom, za svaki tag vracamo niz skracenog taga, ynaci ne moze da bude prazan string
 
  tagsEl.innerHTML = ''
 
@@ -18,4 +26,42 @@ function createTags(input){
   tagEl.innerText = tag
   tagsEl.appendChild(tagEl)
  })
+
+}
+
+function randomSelect(){
+  const times = 30
+
+  const interval = setInterval(() =>{
+      const randomTag = pickRandomTag()
+
+      highlightTag(randomTag)
+
+      setTimeout(() => {
+        unhighlightTag(randomTag)
+      }, 100)
+  },100);
+
+  setTimeout(() =>{
+    clearInterval(interval)
+
+    setTimeout(() =>{
+        const randomTag = pickRandomTag()
+
+        highlightTag(randomTag)
+    }, 100)
+  },times * 100)
+}
+
+function pickRandomTag(){
+  const tags = document.querySelectorAll('.tag')
+  return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function highlightTag(tag){
+    tag.classList.add('highlight')
+}
+
+function unhighlightTag(tag){
+  tag.classList.remove('highlight')
 }
